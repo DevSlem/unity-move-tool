e321c0f1b9e1d75f1836264ca6e977d0e133a1a5
kgmslem <69196755+kgmslem@users.noreply.github.com>
2022-01-17 18:31:46 +0900

    Update MoveToolAvailableAttribute.cs

--- MoveToolAvailableAttribute.cs
+++ MoveToolAvailableAttribute.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 
 /// <summary>
-/// It it's defined for a custom type, you can use MoveToolAttribute to the type field.
+/// If it's defined for a custom type, you can use MoveToolAttribute to the type field.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
 public class MoveToolAvailableAttribute : PropertyAttribute { }
